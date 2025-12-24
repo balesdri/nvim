0f108fddaeebe19892cc0c8161822aeb9cb19ba2
balesdri <daniel.balestri@gmail.com>
2025-12-24 01:02:55 -0600

    Cross functionality VSCode and WSL

--- init.lua
+++ init.lua
@@ -22,18 +22,20 @@
 --x |                                           ,:___  | x |
 --x | -_____                                   /  __ \_| x |
 --x |_/ __  "--..___'_              _  _____.-`  /  \
+--     /  \   _ ___////   ENVRNMT   \( \_____    \__/ _
+--   _ \__/ _                               ""+.   __/ | x |
+--x | \__ _/                                    '. 7 , | x |
+
+local is_vscode = (vim.g.vscode == 1) or (vim.g.vscode == true) 
+local is_wsl = (vim.fn.has("wsl") == 1)
+
+--
+--x |                                           ,:___  | x |
+--x | -_____                                   /  __ \_| x |
+--x |_/ __  "--..___'_              _  _____.-`  /  \
 --     /  \   _ ___////   PLUGINS   \( \_____    \__/ _
 --   _ \__/ _                               ""+.   __/ | x |
 --x | \__ _/                                    '. 7 , | x |
-vim.cmd [[set packpath=~\AppData\Local\nvim\site]]
-vim.cmd [[packadd packer.nvim]]
-
-require('packer').startup(function()
-    use 'wbthomason/packer.nvim' -- Packer can manage itself
-    --use 'folke/which-key.nvim'
-
-    -- Add other plugins here
-end)
 
 if vim.g.vscode then
     -- VSCode extension
@@ -52,49 +54,96 @@ end
 -- Doom-emacs style navigation (NAV TOOL)
 vim.g.mapleader = " "
 
--- [F]ile Management
-    --  Open file 
-vim.api.nvim_set_keymap('n', '<leader><leader>', ':call VSCodeNotify("workbench.action.quickOpen")<CR>', { noremap = true, silent = true })
-    --  f - Search all files in project
-vim.api.nvim_set_keymap('n', '<leader>ff', ':call VSCodeNotify("workbench.action.findInFiles")<CR>', { noremap = true, silent = true })
+if is_vscode then
+    -- [F]ile Management
+        --  Open file 
+    vim.api.nvim_set_keymap('n', '<leader><leader>', ':call VSCodeNotify("workbench.action.quickOpen")<CR>', { noremap = true, silent = true })
+        --  f - Search all files in project
+    vim.api.nvim_set_keymap('n', '<leader>ff', ':call VSCodeNotify("workbench.action.findInFiles")<CR>', { noremap = true, silent = true })
 
 
--- [W]indow Management
-    -- - - Split window down
-vim.api.nvim_set_keymap('n', '<leader>w-', ':call VSCodeNotify("workbench.action.splitEditorDown")<CR>', { noremap = true, silent = true })
-    -- | - Split window right
-vim.api.nvim_set_keymap('n', '<leader>w|', ':call VSCodeNotify("workbench.action.splitEditorRight")<CR>', { noremap = true, silent = true })
-    -- q - Close window
-vim.api.nvim_set_keymap('n', '<leader>wq', ':call VSCodeNotify("workbench.action.closeActiveEditor")<CR>', { noremap = true, silent = true })
+    -- [W]indow Management
+        -- - - Split window down
+    vim.api.nvim_set_keymap('n', '<leader>w-', ':call VSCodeNotify("workbench.action.splitEditorDown")<CR>', { noremap = true, silent = true })
+        -- | - Split window right
+    vim.api.nvim_set_keymap('n', '<leader>w|', ':call VSCodeNotify("workbench.action.splitEditorRight")<CR>', { noremap = true, silent = true })
+        -- q - Close window
+    vim.api.nvim_set_keymap('n', '<leader>wq', ':call VSCodeNotify("workbench.action.closeActiveEditor")<CR>', { noremap = true, silent = true })
 
 
-    -- j - Focus below window
-vim.api.nvim_set_keymap('n', '<leader>wj', ':call VSCodeNotify("workbench.action.focusBelowGroup")<CR>', { noremap = true, silent = true })
-    -- k - Focus above window
-vim.api.nvim_set_keymap('n', '<leader>wk', ':call VSCodeNotify("workbench.action.focusAboveGroup")<CR>', { noremap = true, silent = true })
-    -- h - Focus left window
-vim.api.nvim_set_keymap('n', '<leader>wh', ':call VSCodeNotify("workbench.action.focusLeftGroup")<CR>', { noremap = true, silent = true })
-    -- l - Focus right window
-vim.api.nvim_set_keymap('n', '<leader>wl', ':call VSCodeNotify("workbench.action.focusRightGroup")<CR>', { noremap = true, silent = true })
+        -- j - Focus below window
+    vim.api.nvim_set_keymap('n', '<leader>wj', ':call VSCodeNotify("workbench.action.focusBelowGroup")<CR>', { noremap = true, silent = true })
+        -- k - Focus above window
+    vim.api.nvim_set_keymap('n', '<leader>wk', ':call VSCodeNotify("workbench.action.focusAboveGroup")<CR>', { noremap = true, silent = true })
+        -- h - Focus left window
+    vim.api.nvim_set_keymap('n', '<leader>wh', ':call VSCodeNotify("workbench.action.focusLeftGroup")<CR>', { noremap = true, silent = true })
+        -- l - Focus right window
+    vim.api.nvim_set_keymap('n', '<leader>wl', ':call VSCodeNotify("workbench.action.focusRightGroup")<CR>', { noremap = true, silent = true })
 
 
--- [T]ab Management
-    -- l - Next tab
-vim.api.nvim_set_keymap('n', '<leader>tl', ':call VSCodeNotify("workbench.action.nextEditor")<CR>', { noremap = true, silent = true })
-    -- h - Previous tab
-vim.api.nvim_set_keymap('n', '<leader>th', ':call VSCodeNotify("workbench.action.previousEditor")<CR>', { noremap = true, silent = true })
-    -- q - Close tab
-vim.api.nvim_set_keymap('n', '<leader>tq', ':call VSCodeNotify("workbench.action.closeActiveEditor")<CR>', { noremap = true, silent = true })
+    -- [T]ab Management
+        -- l - Next tab
+    vim.api.nvim_set_keymap('n', '<leader>tl', ':call VSCodeNotify("workbench.action.nextEditor")<CR>', { noremap = true, silent = true })
+        -- h - Previous tab
+    vim.api.nvim_set_keymap('n', '<leader>th', ':call VSCodeNotify("workbench.action.previousEditor")<CR>', { noremap = true, silent = true })
+        -- q - Close tab
+    vim.api.nvim_set_keymap('n', '<leader>tq', ':call VSCodeNotify("workbench.action.closeActiveEditor")<CR>', { noremap = true, silent = true })
 
 
--- [C]ode Management
-    -- c - Comment line
-vim.api.nvim_set_keymap('n', '<leader>cc', ':call VSCodeNotify("editor.action.commentLine")<CR>', { noremap = true, silent = true })
+    -- [C]ode Management
+        -- c - Comment line
+    vim.api.nvim_set_keymap('n', '<leader>cc', ':call VSCodeNotify("editor.action.commentLine")<CR>', { noremap = true, silent = true })
 
 
--- [/]Search
-vim.api.nvim_set_keymap('n', '<leader>/', ':let @/=""<CR>', { noremap = true, silent = true })
+    -- [/]Search
+    vim.api.nvim_set_keymap('n', '<leader>/', ':let @/=""<CR>', { noremap = true, silent = true })
 
+else
+    -- [F]ile Management (GET TELESCOPE)
+        --  Open file 
+    --vim.api.nvim_set_keymap('n', '<leader><leader>', ':find', { noremap = true, silent = true })
+        --  f - Search all files in project
+    vim.api.nvim_set_keymap('n', '<leader>ff', ':vimgrep /', { noremap = true, silent = true })
+
+
+    -- [W]indow Management
+        -- - - Split window down
+    vim.api.nvim_set_keymap('n', '<leader>w-', '<C-w>s', { noremap = true, silent = true })
+        -- | - Split window right
+    vim.api.nvim_set_keymap('n', '<leader>w|', '<C-w>v', { noremap = true, silent = true })
+        -- q - Close window
+    vim.api.nvim_set_keymap('n', '<leader>wq', '<C-w>c', { noremap = true, silent = true })
+
+
+        -- j - Focus below window
+    vim.api.nvim_set_keymap('n', '<leader>wj', '<C-w>j', { noremap = true, silent = true })
+        -- k - Focus above window
+    vim.api.nvim_set_keymap('n', '<leader>wk', '<C-w>k', { noremap = true, silent = true })
+        -- h - Focus left window
+    vim.api.nvim_set_keymap('n', '<leader>wh', '<C-w>h', { noremap = true, silent = true })
+        -- l - Focus right window
+    vim.api.nvim_set_keymap('n', '<leader>wl', '<C-w>l', { noremap = true, silent = true })
+
+
+    -- [T]ab Management
+        -- n - New Tab
+    vim.api.nvim_set_keymap('n', '<leader>tn', '<cmd>tabnew<cr>', { noremap = true, silent = true })
+        -- l - Next tab
+    vim.api.nvim_set_keymap('n', '<leader>tl', '<cmd>tabnext<cr>', { noremap = true, silent = true })
+        -- h - Previous tab
+    vim.api.nvim_set_keymap('n', '<leader>th', '<cmd>tabprevious<cr>', { noremap = true, silent = true })
+        -- q - Close tab
+    vim.api.nvim_set_keymap('n', '<leader>tq', '<cmd>tabclose<cr>', { noremap = true, silent = true })
+
+
+    -- [C]ode Management
+        -- c - Comment line
+    vim.api.nvim_set_keymap('n', '<leader>cc', ':call VSCodeNotify("editor.action.commentLine")<CR>', { noremap = true, silent = true })
+
+
+    -- [/]Search
+    vim.api.nvim_set_keymap('n', '<leader>/', '', { noremap = true, silent = true })
+end
 
 
 --x |                                            ,:___  | x |
